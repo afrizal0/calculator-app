b286f16be87317abbbb3a39f05c5dbdf84caf412
afrizal0 <wikimedicx4504@gmail.com>
2021-07-21 13:47:43 +0800

    make style look better, add a media query, improve the js logic, and add a display result

--- app.js
+++ app.js
@@ -7,24 +7,53 @@ keys.addEventListener('click', e => {
 		const action = key.dataset.action;
 		const keyContent = key.textContent;	
 		const displayNum = displayContent.textContent;
-		const previousKeyType = calculator.dataset.previousKeyType
+		const previousKeyType = calculator.dataset.previousKeyType;
 		if(!action) {
 			if(displayNum === '0' || previousKeyType === 'operator') {
 				displayContent.textContent = keyContent;
+				calculator.dataset.previousKeyType = '';
+
 			}  else {
 				displayContent.textContent = displayNum + keyContent;
 			}
 		} else if(action === "decimal") {
 			displayContent.textContent = displayNum + '.';
 		} else if(action === 'add' || action === 'subtract' || action === 'multiply' || action === 'divide') {
-			key.classList.add('is-pressed');
-			calculator.dataset.previousKeyType = 'operator';
+      		calculator.dataset.firstValue = displayNum;
+      		calculator.dataset.previousKeyType = 'operator';
+			calculator.dataset.operator = action;
+
 		} else if(action === 'calculate') {
 			const firstValue = calculator.dataset.firstValue;
 			const operator = calculator.dataset.operator;
+			const secondValue = displayNum;
 
+			if(firstValue && secondValue && previousKeyType !== 'operator') {
+				displayContent.textContent = calculate(firstValue, operator, secondValue);
+			}
+
+			console.log(firstValue);
+			console.log(operator)
+			console.log(secondValue);
+
+
+		} else if(action === 'clear') {
+			calculator.dataset.firstValue = '0';
+			calculator.dataset.secondValue = '0';
+			displayContent.textContent = '0';
 		}
 	}	
-
-
 })
+const calculate = (n1, operator, n2) => {
+	let result = '';
+	if(operator === 'add') {
+		result =  parseFloat(n1) + parseFloat(n2);
+	} else if(operator === 'subtract') {
+		result = parseFloat(n1) - parseFloat(n2);
+	} else if(operator === 'multiply') {
+		result = parseFloat(n1) * parseFloat(n2);
+	} else if(operator === 'divide') {
+		result = parseFloat(n1) / parseFloat(n2);
+	}
+	return result;
+}
